a6326aafe5e82a24b15b9f9508189a4c42984df1
nazarPryt <ockap90@libero.it>
2022-01-27 22:24:27 +0100

    have fixed burger

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 let header__burger = document.querySelector('.header__burger');
-let header_menu = document.querySelector('.header__inner');
+let header_menu = document.querySelector('.burger__menu');
 let back = document.querySelector('body');
-let header__list = document.querySelector('.burger-list');
+let header__list = document.querySelector('.header__inner-list');
 
 header__burger.onclick = function(){
     header__burger.classList.toggle('header__burger__btn--active');
-    header_menu.classList.toggle('header__inner-list--active');
+    header_menu.classList.toggle('header__burger__btn--active');
     back.classList.toggle('lock');
 }
 
 header__list.onclick = function () {
-    header__list.classList.remove('header__inner-list--active');
+    header__list.classList.remove('header__burger__btn--active');
     back.classList.toggle('lock');
 }
